69bf7924122224b404dd8a1188420b75bc4f21b4
Rico Huijbers <rix0rrr@gmail.com>
2023-02-20 13:19:07 +0100

    Missed dirup

--- packages/@aws-cdk/service-spec-sources/src/loading/load-resource-spec.ts
+++ packages/@aws-cdk/service-spec-sources/src/loading/load-resource-spec.ts
@@ -6,7 +6,7 @@ import { Loader, SchemaValidation } from "./loader";
 export async function loadDefaultResourceSpecification(validate=SchemaValidation.FAIL): Promise<ResourceSpecification> {
   const loader = await Loader.fromSchemaFile<ResourceSpecification>('ResourceSpecification.schema.json', validate);
 
-  const result =  await loader.loadFile(path.join(__dirname, '../../../../sources/CloudFormationResourceSpecification/us-east-1/CloudFormationResourceSpecification.json'));
+  const result =  await loader.loadFile(path.join(__dirname, '../../../../../sources/CloudFormationResourceSpecification/us-east-1/CloudFormationResourceSpecification.json'));
   assertSuccess(result);
   return result;
 }
