b3110e7b539f2bf9334ef31b6eb8f000c3315e33
Momo Kornher <kornherm@amazon.co.uk>
2023-02-23 10:40:21 +0000

    chore: refactor to builder class

--- packages/@aws-cdk/cfn-resources/src/cli/ast.ts
+++ packages/@aws-cdk/cfn-resources/src/cli/ast.ts
@@ -1,49 +1,69 @@
-import { PropertyType, Resource } from '@aws-cdk/service-spec';
-import { Case, InterfaceSpec, MemberKind, TypeReferenceSpec } from '@cdklabs/typewriter';
+import { DatabaseSchema, PropertyType, Resource } from '@aws-cdk/service-spec';
+import { Database } from '@cdklabs/tskb';
+import { Case, InterfaceSpec, InterfaceType, MemberKind, Module, TypeReferenceSpec } from '@cdklabs/typewriter';
 import * as jsii from '@jsii/spec';
+import { ServiceModule } from './service';
 
-export function resourcePropsSpec(r: Resource): InterfaceSpec {
-  const propsInterface = `Cfn${r.name}Props`;
+export class AstBuilder<T extends Module> {
+  /**
+   * @deprecated should be replaced by a new forService once services are available
+   */
+  public static forResource(resource: string, db: Database<DatabaseSchema>): AstBuilder<ServiceModule> {
+    const resourceId = resource.split('::').slice(1).join('.').toLowerCase();
+    const scope = new ServiceModule(resourceId);
 
-  return {
-    export: true,
-    name: propsInterface,
-    kind: jsii.TypeKind.Interface,
-    properties: Object.entries(r.properties).map(([name, p]) => ({
-      kind: MemberKind.Property,
-      name: Case.firstCharToLower(name),
-      type: propertyTypeToTypeReferenceSpec(p.type),
-      immutable: true,
-    })),
-  };
-}
+    return new AstBuilder<ServiceModule>(scope, db);
+  }
 
-export function propertyTypeToTypeReferenceSpec(type: PropertyType): TypeReferenceSpec {
-  switch (type?.type) {
-    case 'string':
-    case 'number':
-    case 'boolean':
-      return {
-        primitive: type.type as jsii.PrimitiveType,
-      };
-    case 'array':
-      return {
-        collection: {
-          kind: jsii.CollectionKind.Array,
-          elementtype: propertyTypeToTypeReferenceSpec(type.element) as any,
-        },
-      };
-    case 'map':
-      return {
-        collection: {
-          kind: jsii.CollectionKind.Map,
-          elementtype: propertyTypeToTypeReferenceSpec(type.element) as any,
-        },
-      };
-    case 'json':
-    default:
-      return {
-        primitive: jsii.PrimitiveType.Any,
-      };
+  protected constructor(public readonly scope: T, public readonly db: Database<DatabaseSchema>) {}
+
+  public addResource(r: Resource) {
+    new InterfaceType(this.scope, this.resourcePropsSpec(r));
+  }
+
+  protected resourcePropsSpec(r: Resource): InterfaceSpec {
+    const propsInterface = `Cfn${r.name}Props`;
+
+    return {
+      export: true,
+      name: propsInterface,
+      kind: jsii.TypeKind.Interface,
+      properties: Object.entries(r.properties).map(([name, p]) => ({
+        kind: MemberKind.Property,
+        name: Case.firstCharToLower(name),
+        type: this.propertyTypeToTypeReferenceSpec(p.type),
+        immutable: true,
+      })),
+    };
+  }
+
+  protected propertyTypeToTypeReferenceSpec(type: PropertyType): TypeReferenceSpec {
+    switch (type?.type) {
+      case 'string':
+      case 'number':
+      case 'boolean':
+        return {
+          primitive: type.type as jsii.PrimitiveType,
+        };
+      case 'array':
+        return {
+          collection: {
+            kind: jsii.CollectionKind.Array,
+            elementtype: this.propertyTypeToTypeReferenceSpec(type.element) as any,
+          },
+        };
+      case 'map':
+        return {
+          collection: {
+            kind: jsii.CollectionKind.Map,
+            elementtype: this.propertyTypeToTypeReferenceSpec(type.element) as any,
+          },
+        };
+      case 'json':
+      default:
+        return {
+          primitive: jsii.PrimitiveType.Any,
+        };
+    }
   }
 }
--- packages/@aws-cdk/cfn-resources/src/cli/main.ts
+++ packages/@aws-cdk/cfn-resources/src/cli/main.ts
@@ -2,13 +2,17 @@ import path from 'path';
 import { TypeScriptRenderer } from '@cdklabs/typewriter';
 import * as fs from 'fs-extra';
 import { loadDatabase } from './db';
-import { moduleFromResource } from './service';
+import { AstBuilder } from './ast';
 
 async function main() {
   const db = await loadDatabase();
   const renderer = new TypeScriptRenderer();
 
-  const services = db.all('resource').map(moduleFromResource);
+  const services = db.all('resource').map((r) => {
+    const ast = AstBuilder.forResource(r.cloudFormationType, db);
+    ast.addResource(r);
+    return ast.scope;
+  });
 
   console.log('Generating %i Resources for %i Services', db.all('resource').length, services.length);
 
