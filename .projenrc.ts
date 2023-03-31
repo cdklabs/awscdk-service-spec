2703f5087a30d91eff7142d68cc3eb9f8f33b739
Momo Kornher <kornherm@amazon.co.uk>
2023-03-31 09:51:43 +0100

    run build fast as well

--- .projenrc.ts
+++ .projenrc.ts
@@ -17,7 +17,7 @@ const repo = new YarnMonorepo({
       trailingComma: pj.javascript.TrailingComma.ALL,
     },
   },
-
+  workflowRunsOn: ['awscdk-service-spec_ubuntu-latest_32-core'],
   gitignore: ['.DS_Store'],
   gitOptions: {
     lfsPatterns: ['sources/**/*.json'],
