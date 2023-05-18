8c20ef8d8f57b6637336909ff3dc959f05ff6f95
Momo Kornher <kornherm@amazon.co.uk>
2023-05-18 14:01:17 +0100

    feat(service-spec-sources): add patches for SAM

--- packages/@aws-cdk/service-spec-sources/src/patches/sam-patches.ts
+++ packages/@aws-cdk/service-spec-sources/src/patches/sam-patches.ts
@@ -1,6 +1,35 @@
 import { normalizeJsonSchema } from './json-schema-patches';
+import { addDefinitions, replaceDefinitionProperty } from './service-patches/core';
+import { JsonObjectLens, Patcher, Reason, makeCompositePatcher, onlyObjects } from '../patching';
+
+const deploymentPreferenceHooks: Patcher<JsonObjectLens> = (lens) => {
+  const hooksTypeReason = Reason.sourceIssue('Use of pattern properties but type is actually well-known.');
+
+  replaceDefinitionProperty(
+    'AWS::Serverless::Function.DeploymentPreference',
+    'Hooks',
+    {
+      $ref: '#/definitions/AWS::Serverless::Function.Hooks',
+    },
+    hooksTypeReason,
+  )(lens);
+
+  addDefinitions(
+    {
+      'AWS::Serverless::Function.Hooks': {
+        type: 'object',
+        properties: {
+          PreTraffic: { type: 'string' },
+          PostTraffic: { type: 'string' },
+        },
+        additionalProperties: false,
+      },
+    },
+    hooksTypeReason,
+  )(lens);
+};
 
 /**
  * Patchers that apply to the SAM Template spec file
  */
-export const patchSamTemplateSpec = normalizeJsonSchema;
+export const patchSamTemplateSpec = makeCompositePatcher(normalizeJsonSchema, onlyObjects(deploymentPreferenceHooks));
