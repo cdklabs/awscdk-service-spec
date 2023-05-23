1bf32e20a328322e6b83b21a3379c057b67f9c40
Momo Kornher <kornherm@amazon.co.uk>
2023-05-23 19:51:22 +0100

    EndpointConfiguration patch

--- packages/@aws-cdk/service-spec-sources/src/patches/sam-patches.ts
+++ packages/@aws-cdk/service-spec-sources/src/patches/sam-patches.ts
@@ -1,9 +1,10 @@
 import { normalizeJsonSchema } from './json-schema-patches';
 import { addDefinitions, replaceDefinitionProperty } from './service-patches/core';
-import { JsonObjectLens, Patcher, Reason, makeCompositePatcher, onlyObjects } from '../patching';
+import { JsonObjectLens, JsonObjectPatcher, Patcher, Reason, makeCompositePatcher, onlyObjects } from '../patching';
+import { jsonschema } from '../types';
 
 const deploymentPreferenceHooks: Patcher<JsonObjectLens> = (lens) => {
-  const hooksTypeReason = Reason.sourceIssue('Use of pattern properties but type is actually well-known.');
+  const reason = Reason.sourceIssue('Use of pattern properties but type is actually well-known.');
 
   replaceDefinitionProperty(
     'AWS::Serverless::Function.DeploymentPreference',
@@ -11,7 +12,7 @@ const deploymentPreferenceHooks: Patcher<JsonObjectLens> = (lens) => {
     {
       $ref: '#/definitions/AWS::Serverless::Function.Hooks',
     },
-    hooksTypeReason,
+    reason,
   )(lens);
 
   addDefinitions(
@@ -25,11 +26,45 @@ const deploymentPreferenceHooks: Patcher<JsonObjectLens> = (lens) => {
         additionalProperties: false,
       },
     },
-    hooksTypeReason,
+    reason,
+  )(lens);
+};
+
+const apiEndpointConfiguration: Patcher<JsonObjectLens> = (lens) => {
+  const reason = Reason.backwardsCompat();
+
+  replaceSamResourceProperty(
+    'AWS::Serverless::Api',
+    'EndpointConfiguration',
+    {
+      anyOf: [{ $ref: '#/definitions/AWS::Serverless::Api.EndpointConfiguration' }, { type: 'string' }],
+    },
+    reason,
   )(lens);
 };
 
 /**
  * Patchers that apply to the SAM Template spec file
  */
-export const patchSamTemplateSpec = makeCompositePatcher(normalizeJsonSchema, onlyObjects(deploymentPreferenceHooks));
+export const patchSamTemplateSpec = makeCompositePatcher(
+  normalizeJsonSchema,
+  onlyObjects(makeCompositePatcher(deploymentPreferenceHooks, apiEndpointConfiguration)),
+);
+
+/**
+ * Replace the property of a SAM Resource.
+ *
+ * NOTE: returns a new patcher. Still needs to be applied to a lens.
+ */
+function replaceSamResourceProperty(
+  resource: string,
+  propertyName: string,
+  newSchema: jsonschema.Schema,
+  reason: Reason,
+): JsonObjectPatcher {
+  return (lens) => {
+    if (lens.jsonPointer === `/definitions/${resource}/properties/Properties/properties/${propertyName}`) {
+      lens.replaceValue(reason.reason, newSchema);
+    }
+  };
+}
