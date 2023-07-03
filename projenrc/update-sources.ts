be52cae5d3cfc0aeea865af4e8fb4cb3a2d79103
Momo Kornher <kornherm@amazon.co.uk>
2023-07-03 10:58:44 +0100

    chore: fix source updates instructions

--- projenrc/update-sources.ts
+++ projenrc/update-sources.ts
@@ -71,6 +71,7 @@ abstract class SourceUpdate extends Component {
       permissions: {
         contents: github.workflows.JobPermission.READ,
         idToken: needsS3Access ? github.workflows.JobPermission.WRITE : github.workflows.JobPermission.NONE,
+        pullRequests: github.workflows.JobPermission.WRITE,
       },
       task: this.task,
       preBuildSteps: [
