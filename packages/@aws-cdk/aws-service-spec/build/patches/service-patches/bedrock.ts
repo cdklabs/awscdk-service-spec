16e62fe0d185c5deecefc55b2b0562d7ef5c84bf
Francis <colifran@amazon.com>
2024-05-01 12:11:17 -0700

    updated verbiage in service patch reason for bedrock storage configuration
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk/aws-service-spec/build/patches/service-patches/bedrock.ts
+++ packages/@aws-cdk/aws-service-spec/build/patches/service-patches/bedrock.ts
@@ -4,7 +4,7 @@ import { patching } from '@aws-cdk/service-spec-importers';
 registerServicePatches(
   forResource('AWS::Bedrock::KnowledgeBase', (lens) => {
     const reason = patching.Reason.sourceIssue(
-      'StorageConfiguration on AWS::Bedrock::KnowledgeBase resource is being dropped while building source model db due to error while importing "oneOf" union type',
+      'StorageConfiguration on AWS::Bedrock::KnowledgeBase resource is being dropped while building source model db due to error importing "oneOf" union type',
     );
     replaceDefinition(
       'StorageConfiguration',
