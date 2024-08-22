b9c6a9e965fd3a65b6b90e234dcfdb11e47352ce
yuanhaoz <yuanhaoz@amazon.com>
2024-08-22 12:20:14 -0700

    fix(bedrock): patch bedrock data source DataSourceConfiguration

--- packages/@aws-cdk/aws-service-spec/build/patches/service-patches/bedrock.ts
+++ packages/@aws-cdk/aws-service-spec/build/patches/service-patches/bedrock.ts
@@ -1,4 +1,4 @@
-import { forResource, registerServicePatches, replaceDefinition } from './core';
+import { addDefinitions, forResource, registerServicePatches, replaceDefinition } from './core';
 import { patching } from '@aws-cdk/service-spec-importers';
 
 registerServicePatches(
@@ -27,6 +27,77 @@ registerServicePatches(
         },
         required: ['Type'],
         additionalProperties: false,
+      },
+      reason,
+    )(lens);
+  }),
+  forResource('AWS::Bedrock::DataSource', (lens) => {
+    const reason = patching.Reason.sourceIssue(
+      'DataSourceConfiguration on AWS::Bedrock::DataSource resource is being dropped for unknown reason but suppoted by CloudForamtion',
+    );
+    addDefinitions(
+      {
+        DataSourceConfiguration: {
+          type: 'object',
+          description: 'Specifies a raw data source location to ingest.',
+          properties: {
+            Type : {
+              "$ref" : "#/definitions/DataSourceType"
+            },
+            S3Configuration: {
+              "$ref" : "#/definitions/S3DataSourceConfiguration"
+            },
+            ConfluenceConfiguration: {
+              "$ref" : "#/definitions/ConfluenceDataSourceConfiguration"
+            },
+            SalesforceConfiguration: {
+              "$ref" : "#/definitions/SalesforceDataSourceConfiguration"
+            },
+            SharePointConfiguration: {
+              "$ref" : "#/definitions/SharePointDataSourceConfiguration"
+            },
+            WebConfiguration: {
+              "$ref" : "#/definitions/WebDataSourceConfiguration"
+            }
+          },
+          required: ['Type'],
+          additionalProperties: false,
+        },
+        'S3DataSourceConfiguration': {
+          type: 'object',
+          description: 'The configuration information to connect to Amazon S3 as your data source.',
+          properties: {
+            BucketArn: {
+              type: 'string',
+              maxLength: 2048,
+              minLength: 1,
+              pattern: "^arn:aws(|-cn|-us-gov):s3:::[a-z0-9][a-z0-9.-]{1,61}[a-z0-9]$",
+              description: "The ARN of the bucket that contains the data source."
+            },
+            InclusionPrefixes: {
+              type: "array",
+              items: {
+                type: "string",
+                maxLength: 300,
+                minLength: 1,
+                description: "Prefix for s3 object."
+              },
+              maxItems: 1,
+              minItems: 1,
+              description: "A list of S3 prefixes that define the object containing the data sources.",
+              insertionOrder: false
+            },
+            BucketOwnerAccountId: {
+              type: "string",
+              maxLength: 12,
+              minLength: 12,
+              pattern: "^[0-9]{12}$",
+              description: "The account ID for the owner of the S3 bucket."
+            }
+          },
+          required: ['BucketArn'],
+          additionalProperties: false
+        }
       },
       reason,
     )(lens);
