15925ef0c484375e242464ef9576315428b76051
Calvin Combs <comcalvi@amazon.com>
2024-10-22 09:58:14 -0700

    tests

---
+++ packages/@cdklabs/typewriter/test/exports.test.ts
@@ -0,0 +1,48 @@
+import { FreeFunction, Module, TypeScriptRenderer } from '../src';
+
+const renderer = new TypeScriptRenderer();
+let scope: Module;
+
+beforeEach(() => {
+  scope = new Module('typewriter.test');
+});
+
+describe('functions', () => {
+  test('functions can be implicitly not exported', () => {
+    new FreeFunction(scope, {
+      name: 'freeFunction',
+    });
+
+    expect(renderer.render(scope)).toMatchInlineSnapshot(`
+      "/* eslint-disable prettier/prettier,max-len */
+      // @ts-ignore TS6133
+      function freeFunction(): void;"
+    `);
+  });
+
+  test('functions can be explicitly exported', () => {
+    new FreeFunction(scope, {
+      name: 'freeFunction',
+      export: true,
+    });
+
+    expect(renderer.render(scope)).toMatchInlineSnapshot(`
+      "/* eslint-disable prettier/prettier,max-len */
+      // @ts-ignore TS6133
+      export function freeFunction(): void;"
+    `);
+  });
+
+  test('functions can be explicitly not exported', () => {
+    new FreeFunction(scope, {
+      name: 'freeFunction',
+      export: false,
+    });
+
+    expect(renderer.render(scope)).toMatchInlineSnapshot(`
+      "/* eslint-disable prettier/prettier,max-len */
+      // @ts-ignore TS6133
+      function freeFunction(): void;"
+    `);
+  });
+});
