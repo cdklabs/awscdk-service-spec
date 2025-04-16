ee340240f83c900fb1e93fc19c2543f7774ab094
Paul Sun <paulhcs@amazon.com>
2025-04-16 12:23:25 -0700

    patchResource to add back description

--- packages/@aws-cdk/aws-service-spec/build/patches/service-patches/rds.ts
+++ packages/@aws-cdk/aws-service-spec/build/patches/service-patches/rds.ts
@@ -1,10 +1,22 @@
 import { fp, registerServicePatches } from './core';
-import { patching } from '@aws-cdk/service-spec-importers';
+import { patching, types } from '@aws-cdk/service-spec-importers';
 
 registerServicePatches(
   fp.addReadOnlyProperties(
     'AWS::RDS::DBCluster',
     ['ReadEndpoint'],
     patching.Reason.sourceIssue('ReadEndpoint should be listed in readOnlyProperties.'),
+  ),
+  fp.patchResourceAt<types.CloudFormationRegistryResource['readOnlyProperties']>(
+    'AWS::DMS::ReplicationConfig',
+    '/properties/CertificateDetails',
+    patching.Reason.sourceIssue('Missing description caused property to be removed in L1 update.'),
+    (descriptionAndType = {}) => {
+      descriptionAndType = {
+        "$ref" : "#/definitions/CertificateDetails",
+        "description" : "The details of the DB instance’s server certificate."
+      }
+      return descriptionAndType;
+    },
   ),
 );
