4f9ae9df7c8d1e35e92e02e400ec5a0e286e5b92
Rico Hermans <rix0rrr@gmail.com>
2025-10-30 16:34:24 +0100

    fix(typewriter): object literals contain unnecessary quotes (#2149)
    Object keys are now always rendered with `"quotes"`, even if they're
    unnecessary. Do that only conditionally.
    
    ---------
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/@cdklabs/typewriter/test/expressions.test.ts
+++ packages/@cdklabs/typewriter/test/expressions.test.ts
@@ -1,4 +1,34 @@
-import { expr, Expression, Module, stmt, TypeScriptRenderer } from '../src';
+import { AnonymousInterfaceImplementation, expr, Expression, Module, stmt, TypeScriptRenderer } from '../src';
+
+test('object literal', () => {
+  const ex = expr.object({
+    a: expr.lit(1),
+    b: expr.lit(2),
+  });
+
+  // eslint-disable-next-line prettier/prettier
+  expect(renderExpr(ex)).toMatch([
+    '{',
+    '  a: 1,',
+    '  b: 2',
+    '}',
+  ].join('\n'));
+});
+
+test('anonymous interface implementation', () => {
+  const ex = new AnonymousInterfaceImplementation({
+    a: expr.lit(1),
+    b: expr.lit(2),
+  });
+
+  // eslint-disable-next-line prettier/prettier
+  expect(renderExpr(ex)).toMatch([
+    '{',
+    '  a: 1,',
+    '  b: 2',
+    '}',
+  ].join('\n'));
+});
 
 test('splat', () => {
   const ex = expr.splat(expr.ident('input'));
@@ -11,5 +41,15 @@ function renderExpr(ex: Expression) {
   scope.addInitialization(stmt.expr(ex));
 
   const renderer = new TypeScriptRenderer();
-  return renderer.render(scope);
+  let ret = renderer.render(scope);
+
+  // Disregard eslint directives
+  if (ret.startsWith('/* eslint-disable')) {
+    ret = ret.split('\n').slice(1).join('\n');
+  }
+  // Remove trailing ;
+  if (ret.endsWith(';')) {
+    ret = ret.slice(0, -1);
+  }
+  return ret;
 }
